80b3ef564ccf386da5d0367d6058fe11c05aff41
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-12-29 01:19:03 +0600

    Create 11.stl_unorderset.cpp

---
+++ 11.stl_unorderset.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//so basically it is simmillar to set
+//but the difference is it doesn't store in a sorted way
+//but it does stores unique values
+
+void learn_unorder_set(){
+//the upperbound and the lowerbound functions doesnt work here
+//other all functions are same to set
+
+
+unordered_set<int>alvi;
+
+
+
+}
+int main(){
+    learn_unorder_set();
+    return 0;
+}
