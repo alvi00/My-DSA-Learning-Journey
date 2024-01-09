ff9f61200da99ae9967a1c105f825e52fbdd5180
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-10 01:28:50 +0600

    Update 2.bubble_sort.cpp

--- 4.Sorting/2.bubble_sort.cpp
+++ 4.Sorting/2.bubble_sort.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    cout<<"Enter a size of the array"<<endl;
+    cout << "Enter a size of the array" << endl;
     int n;
     cin >> n;
     int arr[n];
-     cout<<"Give the elements"<<endl;
+    cout << "Give the elements" << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
@@ -14,11 +14,18 @@ int main()
 
     for (int i = n - 1; i >= 1; i--)
     {
+        int didswap = 0;
         for (int j = 0; j <= i - 1; j++)
+        {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                didswap = 1;
             }
+        }
+        if(didswap==0){
+            break;
+        }
     }
 
     for (int i = 0; i < n; i++)
