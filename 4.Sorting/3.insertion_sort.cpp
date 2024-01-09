365599320305f3a91fa9584d2c36389d2f607057
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-10 02:59:57 +0600

    Create 3.insertion_sort.cpp

---
+++ 4.Sorting/3.insertion_sort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Enter a size of the array" << endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Give the elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+}
