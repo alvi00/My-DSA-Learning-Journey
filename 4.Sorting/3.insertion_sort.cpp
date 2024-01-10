a8f2c39d5f706dc8ee3c981bbc04073b106768d5
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-11 00:32:57 +0600

    Update 3.insertion_sort.cpp

--- 4.Sorting/3.insertion_sort.cpp
+++ 4.Sorting/3.insertion_sort.cpp
@@ -12,16 +12,23 @@ int main()
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        int j = i;
-        while (j > 0 && arr[j - 1] > arr[j])
-        {
-            swap(arr[j - 1], arr[j]);
-            j--;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int j = i;
+    //     while (j > 0 && arr[j - 1] > arr[j])
+    //     {
+    //         swap(arr[j - 1], arr[j]);
+    //         j--;
+    //     }
+    // }
+
+    for(int i=0;i<=n-1;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j-1]>arr[j]){
+                swap(arr[j-1],arr[j]);
+            }
         }
     }
-
     for (int i = 0; i < n; i++)
     {
         cout << arr[i];
