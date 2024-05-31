b86cb8bcde5525ddab4a997f84d2038dd74826a3
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-06-01 02:27:29 +0600

    Update main.cpp

--- 5.LinkLIst/Sorted_unsorted_linked_list_with_class_And_obj/main.cpp
+++ 5.LinkLIst/Sorted_unsorted_linked_list_with_class_And_obj/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include"header.h"
-using namespace std;
 
 int main()
 {
-    link_list alvi;
-    alvi.insert_item_sort(5);
-    alvi.insert_item_sort(3);
-    alvi.insert_item_sort(22);
-    alvi.insert_item_sort(10);
-    //alvi.delete_item(22);
-    //alvi.display();
-    alvi.resetlist();
-    for(int i=0;i<4;i++){
-        int a;
-        alvi.getnextitem(a);
-        cout<<a<<" ";
-    }
+    linked_list l1;
+    l1.insert_at_begin(10);
+    l1.insert_at_end(20);
+    l1.insert_at_end(40);
+    l1.insert_at_end(50);
+    l1.insert_at_pos(3,30);
+    l1.insert_at_end(60);
+    l1.insert_at_end(70);
+    l1.insert_at_end(80);
+    l1.delete_at_begin();
+    l1.delete_at_pos(3);
+    l1.delete_at_end();
+    l1.insert(40);
+    l1.delete_num(30);
+    l1.display();
     return 0;
 }
