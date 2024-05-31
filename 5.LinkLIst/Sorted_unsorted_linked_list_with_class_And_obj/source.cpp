23f71b932509949cf2f3139939900154d8642243
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-06-01 02:27:51 +0600

    Update source.cpp

--- 5.LinkLIst/Sorted_unsorted_linked_list_with_class_And_obj/source.cpp
+++ 5.LinkLIst/Sorted_unsorted_linked_list_with_class_And_obj/source.cpp
@@ -1,61 +1,151 @@
 #include "header.h"
-#include<bits/stdc++.h>
-using namespace std;
-Node::Node(int a){
+Node::Node(int a)
+{
     data=a;
     next=nullptr;
 }
 
-link_list::link_list(){
+linked_list::linked_list()
+{
     head=nullptr;
-    length=0;
-    currentpos=nullptr;
 }
 
-link_list::~link_list(){
-    makeempty();
-}
-void link_list::inset_item_begin(int a){
-    Node *temp=new Node(a);
-    temp->next=head;
-    head=temp;
-}
-
-void link_list::insert_item_end(int a){
+void linked_list::insert_at_begin(int a)
+{
     Node *newnode=new Node(a);
+    newnode->next=head;
+    head=newnode;
+}
+
+void linked_list::insert_at_pos(int p,int a)
+{
+    Node *newnode=new Node(a);
+
+    if(p==1)
+    {
+        newnode->next=head;
+        head=newnode;
+    }
+    else
+    {
+        Node *prev=nullptr;
+        Node *temp=head;
+
+        int count=1;
+        while(temp!=nullptr)
+        {
+            if(count==p)
+            {
+                prev->next=newnode;
+                newnode->next=temp;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+            count++;
+        }
+    }
+}
+
+void linked_list::insert_at_end(int a)
+{
+    Node*newnode=new Node(a);
     Node *temp=head;
-    while(temp->next=nullptr){
+    while(temp->next!=nullptr)
+    {
         temp=temp->next;
     }
     temp->next=newnode;
 }
 
-void link_list::insert_item_sort(int a){
-    Node *newnode=new Node(a);
-    if(head==nullptr || head->data>a){
-        newnode->next=head;
-        head=newnode;
-    }
-    else{
-            Node *temp=head;
-    while(temp->next!=nullptr&& temp->next->data<a){
-       temp=temp->next;
-    }
-     newnode->next=temp->next;
-        temp->next=newnode;
-    }
-}
-void link_list::delete_item(int a){
+void linked_list::delete_at_begin()
+{
     Node *temp=head;
-
-    if(temp->data==a){
+    head=head->next;
+    delete temp;
+}
+void linked_list::delete_at_pos(int p)
+{
+    if(p==1)
+    {
+        Node *temp=head;
         head=head->next;
         delete temp;
     }
-    else{
+    else
+    {
+        int count=1;
         Node *prev=nullptr;
-        while(temp!=nullptr){
-            if(temp->data==a){
+        Node *temp=head;
+        while(temp!=nullptr)
+        {
+            if(p==count)
+            {
+                prev->next=prev->next->next;
+                delete temp;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+            count++;
+        }
+    }
+}
+
+void linked_list::delete_at_end()
+{
+    Node *temp=head;
+    while(temp->next->next!=nullptr)
+    {
+        temp=temp->next;
+    }
+    delete temp->next;
+    temp->next=nullptr;
+}
+
+void linked_list::display()
+{
+    Node *temp=head;
+    while(temp!=nullptr)
+    {
+        cout<<temp->data<<"    ";
+        temp=temp->next;
+    }
+}
+void linked_list::insert(int a)
+{
+    Node *newnode=new Node(a);
+    if(head==nullptr)
+    {
+        head=newnode;
+    }
+    else
+    {
+        Node *temp=head;
+        while(temp->next!=nullptr)
+        {
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+}
+
+void linked_list::delete_num(int a)
+{
+    Node *temp=head;
+    if(temp->data==a)
+    {
+        head=head->next;
+        delete temp;
+    }
+    else
+    {
+        Node *prev=nullptr;
+        temp=head;
+        while(temp!=nullptr)
+        {
+            if(temp->data==a)
+            {
                 prev->next=prev->next->next;
                 delete temp;
                 break;
@@ -63,48 +153,8 @@ void link_list::delete_item(int a){
             prev=temp;
             temp=temp->next;
         }
-    }
-}
 
-void link_list::makeempty(){
-    Node *temp=head;
-    while(head!=nullptr){
-        head=head->next;
-        delete temp;
-        temp=head;
-    }
-}
-
-bool link_list::isfull(){
-    try{
-        Node *temp=new Node(0);
-        delete temp;
-        return false;
-
-    }catch(bad_alloc exception){
-        return true;
     }
 
 }
 
-void link_list::display(){
-    Node *temp=head;
-    while(temp!=nullptr){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-}
-
-void link_list::getnextitem(int&a){
-    if(currentpos==nullptr){
-        currentpos=head;
-    }
-    else{
-        currentpos=currentpos->next;
-    }
-    a=currentpos->data;
-}
-
-void link_list::resetlist(){
-    currentpos=nullptr;
-}
