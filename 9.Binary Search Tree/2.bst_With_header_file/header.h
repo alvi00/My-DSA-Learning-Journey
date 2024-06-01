a443154156fef3c704275525fad58915d8f051a9
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-06-02 04:15:01 +0600

    Update header.h

--- 9.Binary Search Tree/2.bst_With_header_file/header.h
+++ 9.Binary Search Tree/2.bst_With_header_file/header.h
@@ -1,28 +1,37 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 class Node
 {
 public:
     int data;
     Node *left;
     Node *right;
-    Node(int a)
-    {
-        data=a;
-        left=nullptr;
-        right=nullptr;
-    }
-
-    void insert(int);
-    void search(Node *root,int);
-    void print(Node *root);
-
-    Node* deleteNode(Node* root, int key);
-    int lengthis(Node *root);
-
+    Node(int a);
 };
 
+class BinaryTree
+{
+public:
+    BinaryTree();
+    ~BinaryTree();
+    void insert(int value);
+    void search(int value);
+    void print();
+    void deleteNode(int key);
+    int lengthis();
+
+private:
+    Node* root;
+    void insert(Node* &root, int value);
+    void search(Node* root, int value);
+    void print(Node* root);
+    Node* deleteNode(Node* root, int key);
+    int countNodes(Node* root);
+    void makeEmpty(Node* &root);
+    Node* findMin(Node* root);
+};
 
 #endif // HEADER_H_INCLUDED
