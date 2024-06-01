6379c52224bdab16a1c5760c93bc13ac749ddacf
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-06-02 04:15:17 +0600

    Update main.cpp

--- 9.Binary Search Tree/2.bst_With_header_file/main.cpp
+++ 9.Binary Search Tree/2.bst_With_header_file/main.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int main()
 {
-    Node *root=new Node(8);
-    root->insert(10);
-    root->insert(20);
-    root->insert(30);
-    root->search(root, 8); // Call the search method to search for the value 51 in the tree
-    root->search(root, 5); // Call the search method to searc
-    root->print(root);
-    root=root->deleteNode(root,10);
-    cout<<endl;
-    root->print(root);
-    int a=root->lengthis(root);
-    cout<<a;
+    BinaryTree tree;
+
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(30);
+    tree.search(8); // Call the search method to search for the value 8 in the tree
+    tree.search(5); // Call the search method to search for the value 5 in the tree
+    tree.print();
+    cout << endl;
+    tree.deleteNode(10);
+    tree.print();
+    cout << endl;
+    int a = tree.lengthis();
+    cout << a;
     return 0;
 }
